4b0c1e457a8758ef05649db32c631d275c9e426f
whitedark9 <blancooscuro.mm@gmail.com>
2024-05-12 09:13:53 +0630

    small change

--- src/constants/app.config.ts
+++ src/constants/app.config.ts
@@ -1,8 +1,8 @@
 const APP = {
   name: "Nextra 2",
   shortName: "Nextra 2",
-  domain: "https://www.github.com/nextra-2-template",
-  description: "nextra-2-template for me",
+  domain: "https://www.github.com/nextra-2",
+  description: "nextra-2-template",
   themes: ["light", "dark", "system"],
   defaultTheme: "system",
   themeStorageKey: "@theme",
